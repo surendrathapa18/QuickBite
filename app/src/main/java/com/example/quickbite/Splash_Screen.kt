70dfee68bf875d52ea9ef71fdf7b75237649d8a3
Surendra Thapa <surendrathapa@isu.edu>
2026-02-20 13:35:22 -0700

    Login Page Update

--- app/src/main/java/com/example/quickbite/Splash_Screen.kt
+++ app/src/main/java/com/example/quickbite/Splash_Screen.kt
@@ -3,18 +3,19 @@ package com.example.quickbite
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@Suppress("DEPRECATION")
+
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
             finish()
