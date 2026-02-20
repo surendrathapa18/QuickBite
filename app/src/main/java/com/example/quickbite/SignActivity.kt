463fb47abe72b5398085fef57dd8613b40fb50ae
Surendra Thapa <surendrathapa@isu.edu>
2026-02-20 13:39:18 -0700

    Update Sign Page

---
+++ app/src/main/java/com/example/quickbite/SignActivity.kt
@@ -0,0 +1,29 @@
+package com.example.quickbite
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quickbite.databinding.ActivitySignBinding
+
+class SignActivity : AppCompatActivity() {
+    private val binding : ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.alreadyhavebutton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
